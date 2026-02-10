b8d93de0f2fbd782262ac74c5ba5d704cc4858e3
Akmal <akmalajmal2@gmail.com>
2026-02-10 08:18:01 +0530

    refactor(Frontend): adding render url

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -16,7 +16,7 @@ const Button: React.FC<ButtonProps> = memo(
     className = "",
     ...props
   }) => {
-    const btnClassName = `rounded-xl px-4 py-2 leading-none text-[#fff] ${variant === "primary" ? "bg-blue-500" : variant === "danger" ? "bg-orange-500" : ""} ${size === "lg" ? "text-2xl font-bold px-6 py-3 rounded-2xl" : size === "md" ? "text-xl font-semibold px-3 py-2 rounded-xl" : ""} ${className}`;
+    const btnClassName = `rounded-xl px-4 py-2 leading-none text-[#fff] cursor-pointer ${variant === "primary" ? "bg-blue-500" : variant === "danger" ? "bg-orange-500" : ""} ${size === "lg" ? "text-2xl font-bold px-6 py-3 rounded-2xl" : size === "md" ? "text-xl font-semibold px-3 py-2 rounded-xl" : ""} ${className}`;
 
     return (
       <button className={btnClassName} {...props}>
